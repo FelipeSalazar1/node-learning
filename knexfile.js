c1ffecff3f1c772babdf2d231d971f341aed5739
Felipe <felipe.salazar2004@outlook.com>
2024-02-07 23:34:53 -0300

    Commit

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: path.resolve(
+        __dirname,
+        "API",
+        "src",
+        "database",
+        "database.db"
+      ),
+    },
+    migrations: {
+      directory: path.resolve(
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
+    },
+    useNullAsDefault: true,
+  },
+};
